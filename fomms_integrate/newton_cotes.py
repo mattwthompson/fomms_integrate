fde45255e1124835e088a170c4697a6825d11fcf
Matthew W. Thompson <matt.thompson@vanderbilt.edu>
2018-07-19 15:18:03 -0500

    Fix typo

---
+++ fomms_integrate/newton_cotes.py
@@ -0,0 +1,26 @@
+"""
+This file contains the implementation of the trapezoidal rule
+"""
+
+import numpy as np
+
+def trapz(x, f):
+    """ 
+    Compute a 1D definite integral using the trapezoidal rule
+    Parameters
+    ----------
+    f : function
+        User defined function.
+    x : numpy array
+        Integration domain.
+    Returns
+    -------
+    I : float
+        Integration result.
+    """   
+    a = x[0]
+    b = x[1]
+    ya = f(a)
+    yb = f(b)
+    I = (b-a) * (ya + yb) / 2
+    return I
